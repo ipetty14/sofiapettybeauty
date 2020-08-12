2caa77eb0313aee983fa966155dbd11ded021c91
Ian Petty <ipetty@masonite.com>
2020-08-12 18:51:18 -0400

    Update main logo

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import SVG from '../../assets/logos/Slice 1.svg';
+import SVG from '../../assets/logos/Slice 1-1.png';
 
 const Img = styled.img`
   width: 100%;
   max-width: 600px;
 `;
 
-const Image = () => <Img src={SVG} alt="GatsbyJS + Storybook" />;
+const Image = () => <Img src={SVG} alt="Sofia Petty Beauty" />;
 
 export default Image;
--- src/global/GlobalStyle.js
+++ src/global/GlobalStyle.js
@@ -4,6 +4,11 @@ import reset from 'styled-reset-advanced';
 export default createGlobalStyle`
   ${reset};
 
+  @font-face {
+  font-family: "Lato";
+  src: url("../../static/fonts/lato/Lato-Regular.woff");
+  } 
+
   body {
     font-family: 'Avenir Next', 'Helvetica Neue', 'Helvetica', sans-serif;
     font-weight: 500;
